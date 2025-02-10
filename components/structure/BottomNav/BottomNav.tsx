69c21ec428bfffa11b602772fad91fe7ffebaf75
Equilibria Labs <carmen@equilibrialabs.com>
2025-02-10 17:37:54 +0000

    Centered Nav

--- components/structure/BottomNav/BottomNav.tsx
+++ components/structure/BottomNav/BottomNav.tsx
@@ -23,7 +23,7 @@ export default function BottomNav({
 
   return (
     <nav className='fixed bottom-0 left-0 right-0 bg-foreground text-background'>
-      <div className='flex justify-around items-center p-4'>
+      <div className='flex justify-around items-center p-4  max-w-maxWidth mx-auto'>
         {items.map(item => (
           <BottomNavItem
             key={item.label}
