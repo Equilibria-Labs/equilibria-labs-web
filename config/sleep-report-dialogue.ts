43e11878938fd9d063ecff8834198c96481ac67f
Equilibria Labs <carmen@equilibrialabs.com>
2025-02-11 21:02:52 +0000

    completed sleep steps

--- config/sleep-report-dialogue.ts
+++ config/sleep-report-dialogue.ts
@@ -7,7 +7,7 @@ export type SleepReportDialogueConfig = {
 export const sleepReportDialogueConfig: SleepReportDialogueConfig = {
   steps: [
     {
-      id: '1a',
+      id: 'question-1a',
       description: 'How difficult is it for you to fall asleep?',
       type: 'single-choice',
       question: 'How difficult is it for you to *fall asleep*?',
@@ -21,7 +21,7 @@ export const sleepReportDialogueConfig: SleepReportDialogueConfig = {
       ],
     },
     {
-      id: '1b',
+      id: 'question-1b',
       description: 'How difficult is it for you to stay asleep?',
       type: 'single-choice',
       question: 'How difficult is it for you to *stay asleep*?',
@@ -34,9 +34,8 @@ export const sleepReportDialogueConfig: SleepReportDialogueConfig = {
         { id: 'extremely-difficult', text: 'Extremely difficult', value: 4 },
       ],
     },
-
     {
-      id: '1c',
+      id: 'question-1c',
       description: 'How severe are problems waking too early?',
       type: 'single-choice',
       question: 'How severe are problems *waking too early*?',
@@ -50,55 +49,62 @@ export const sleepReportDialogueConfig: SleepReportDialogueConfig = {
       ],
     },
     {
-      id: 'question-4',
+      id: 'question-2',
+      description:
+        'How satisfied/dissatisfied are you with your currentsleep pattern?',
+      instruction: 'In the past 2 weeks',
       type: 'single-choice',
       question:
-        'How satisfied/dissatisfied are you with your sleep pattern in the last 2 weeks?',
+        'How *satisfied/dissatisfied* are you with your current sleep pattern?',
       choices: [
-        { id: 'very-satisfied', text: 'Very Satisfied' },
-        { id: 'satisfied', text: 'Satisfied' },
-        { id: 'moderately-satisfied', text: 'Moderately Satisfied' },
-        { id: 'dissatisfied', text: 'Dissatisfied' },
-        { id: 'very-dissatisfied', text: 'Very Dissatisfied' },
+        { id: 'satisfied', text: 'Satisfied', value: 0 },
+        { id: 'moderately-satisfied', text: 'Moderately satisfied', value: 1 },
+        { id: 'neutral', text: 'Neutral', value: 2 },
+        { id: 'dissatisfied', text: 'Dissatisfied', value: 3 },
+        { id: 'very-dissatisfied', text: 'Very Dissatisfied', value: 4 },
       ],
     },
     {
-      id: 'question-5',
+      id: 'question-3',
+      description:
+        'How much does your sleep problem interfere with your daily functioning?',
       type: 'single-choice',
       question:
-        'How noticeable to others do you think your sleep problem is in terms of impairing the quality of your life?',
+        'How much does your sleep problem *interfere* with your daily functioning?',
+      instruction: 'Fatigue, concentration, memory, mood, etc.',
       choices: [
-        { id: 'not-at-all', text: 'Not at all Noticeable' },
-        { id: 'a-little', text: 'A Little' },
+        { id: 'not-at-all', text: 'Not at all interfering', value: 0 },
+        { id: 'a-little', text: 'A little interfering', value: 1 },
+        { id: 'somewhat', text: 'Somewhat interfering', value: 2 },
+        { id: 'much', text: 'Much interfering', value: 3 },
+        { id: 'very-much', text: 'Very much interfering', value: 4 },
+      ],
+    },
+    {
+      id: 'question-4',
+      type: 'single-choice',
+      question:
+        'How *noticeable* to others do you think your sleep problem is?',
+      instruction: 'In terms of impairing the quality of your life',
+      choices: [
+        { id: 'not-at-all', text: 'Not at all noticeable at all' },
+        { id: 'barely', text: 'Barely' },
         { id: 'somewhat', text: 'Somewhat' },
         { id: 'much', text: 'Much' },
         { id: 'very-much', text: 'Very Much Noticeable' },
       ],
     },
     {
-      id: 'question-6',
+      id: 'question-5',
       type: 'single-choice',
       question:
-        'How worried/distressed are you about your current sleep problem?',
+        'How *worried/distressed* are you about your current sleep problem?',
       choices: [
         { id: 'not-at-all', text: 'Not at all Worried' },
         { id: 'a-little', text: 'A Little' },
         { id: 'somewhat', text: 'Somewhat' },
         { id: 'much', text: 'Much' },
         { id: 'very-much', text: 'Very Much Worried' },
-      ],
-    },
-    {
-      id: 'question-7',
-      type: 'single-choice',
-      question:
-        'To what extent do you consider your sleep problem to interfere with your daily functioning (e.g. ability to function at work) in the last 2 weeks?',
-      choices: [
-        { id: 'not-at-all', text: 'Not at all Interfering' },
-        { id: 'a-little', text: 'A Little' },
-        { id: 'somewhat', text: 'Somewhat' },
-        { id: 'much', text: 'Much' },
-        { id: 'very-much', text: 'Very Much Interfering' },
       ],
     },
   ],
