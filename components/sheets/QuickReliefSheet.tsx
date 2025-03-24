c06d515e42e94926a0a77d764af508e00dab5221
Adam Morley <carmen@equilibrialabs.com>
2025-03-24 22:47:20 +0000

    Style bottom sheet

--- components/sheets/QuickReliefSheet.tsx
+++
@@ -1,6 +0,0 @@
-import SheetNav from '@/components/navigation/SheetNav';
-import { quickReliefNavConfig } from '@/config/navigation';
-
-export default function QuickReliefSheet() {
-  return <SheetNav config={quickReliefNavConfig} />;
-}
