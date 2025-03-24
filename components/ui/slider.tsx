1f542f17b14186ee33dc290cbdb43e6059e785d8
Adam Morley <carmen@equilibrialabs.com>
2025-03-24 22:48:53 +0000

    pointer on slider dot

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -20,7 +20,7 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className='relative h-2 w-full grow overflow-hidden rounded-full bg-secondary/40'>
       <SliderPrimitive.Range className='absolute h-full bg-secondary' />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className='block h-10 w-10 rounded-full border-2 bg-secondary ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50' />
+    <SliderPrimitive.Thumb className='block h-10 w-10 rounded-full border-2 bg-secondary ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer' />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
